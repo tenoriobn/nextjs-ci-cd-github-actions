ac910617eaa2eec3042df5c05e3ec59ffc31230e
Bruno Tenório <tenoriobn@gmail.com>
2024-12-06 19:42:35 -0300

    update

--- scripts/pullRequestPreview.js
+++ scripts/pullRequestPreview.js
@@ -3,8 +3,11 @@ import { execSync } from 'child_process'
 console.log('[DEPLOY_PREVIEW]: START');
 const command = "yarn deploy:staging ";
 const output = execSync(command, { encoding: "utf8" });
-console.log(output)
-console.log('[DEPLOY_PREVIEW]: END');
+
+const outputLines = output.split("\n");
+const DEPLOY_URL = outputLines[outputLines.length -1];
+
+console.log(`You can see the deploy preview on: ${DEPLOY_URL}`);
 
 console.log('[GITHUB_COMMENT]: START');
 console.log('[GITHUB_COMMENT]: END');
