23cb9fddc9d723ecd30a80f0510a92bdf54f8bb5
Bruno Tenório <tenoriobn@gmail.com>
2024-12-10 15:00:06 -0300

    corrigindo HomeScreen

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import NextLink from "next/link";
 
 export default function HomeScreen() {
-return (
-<div>
-<h1>Página Inicial [Nova Alteração!]</h1>
-<NextLink href="/sobre">
-  <a>Ir para a sobre</a>
-</NextLink>
-</div>
-);
+  return (
+    <div>
+      <h1>Página Inicial [Nova Alteração!]</h1>
+      <NextLink href="/sobre">
+        <a>Ir para a sobre</a>
+      </NextLink>
+    </div>
+  );
 }
